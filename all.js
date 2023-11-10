80fa04021e9cc531abc98beeca6c4dd9ec338d57
Peggy Lin <stu90233@gmail.com>
2023-11-10 21:05:10 +0800

    feat: 🎸 use axios

--- all.js
+++ all.js
@@ -1,40 +1,20 @@
-let data = [
-  {
-    id: 0,
-    name: "肥宅心碎賞櫻3日",
-    imgUrl:
-      "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-    area: "高雄",
-    description: "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-    group: 87,
-    price: 1400,
-    rate: 10,
-  },
-  {
-    id: 1,
-    name: "貓空纜車雙程票",
-    imgUrl:
-      "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    area: "台北",
-    description:
-      "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-    group: 99,
-    price: 240,
-    rate: 2,
-  },
-  {
-    id: 2,
-    name: "台中谷關溫泉會1日",
-    imgUrl:
-      "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    area: "台中",
-    description:
-      "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-    group: 20,
-    price: 1765,
-    rate: 7,
-  },
-];
+let data;
+
+axios
+  .get(
+    `https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json`
+  )
+  .then((res) => {
+    data = res.data.data;
+
+    renderCards(data);
+    getRegionOptions();
+    renderSearchRegion();
+    getTotalNum(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // 渲染畫面
 function renderCards(data) {
@@ -77,7 +57,6 @@ function renderCards(data) {
 
   ticketCardArea.innerHTML = ticketCards;
 }
-renderCards(data);
 
 // 取得地區選項
 let ticketRegionList = [];
@@ -88,7 +67,6 @@ function getRegionOptions() {
     }
   });
 }
-getRegionOptions();
 
 // 渲染景點地區選項
 const ticketRegionSelect = document.querySelector("#ticketRegion");
@@ -137,8 +115,6 @@ function renderSearchRegion() {
   regionSearchSelect.innerHTML = regionSearchOptions;
 }
 
-renderSearchRegion();
-
 // 地區篩選功能
 const regionSearch = document.querySelector(".regionSearch");
 let newData;
@@ -163,7 +139,6 @@ function getTotalNum(data) {
   // data;
   searchResultText.textContent = `本次搜尋共 ${totalNum} 筆資料`;
 }
-getTotalNum(data);
 
 // 監聽表格內容變化
 let ticketName = document.querySelector("#ticketName");
